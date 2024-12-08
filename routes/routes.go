934cb589275f1abdd3714207bb4191936f481c9c
Alan Freeman <afreeman@iwgvt.org>
2024-12-08 08:27:49 -0600

    Removed redundant setprice code

--- routes/routes.go
+++ routes/routes.go
@@ -30,7 +30,6 @@ func AuthRoutes() {
 	Router.HandleFunc("/auth/registerauth", controllers.RegisterAuthHandler)
 
 	// Process user input of new price
-	Router.HandleFunc(`/auth/setprice`, controllers.SetPricePostHandler)
 	Router.HandleFunc(`/auth/setprices`, controllers.SetPricesAuthHandler)
 	Router.HandleFunc(`/auth/setpriceshandle`, controllers.SetPricesPostHandler)
 
